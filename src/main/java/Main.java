67947ba8a8e7a9f3d2869e76fb21e9ceeb3115d8
Nikolai-Zakharov <nzakharov24@yandex.ru>
2022-07-29 21:15:57 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 1000_60;
+    boolean registered = true;
+
+    service.calculate(amount, registered);
+  }
+}
+
